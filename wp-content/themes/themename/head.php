996b2ff2de44b25f0db99ab8b7573538d5d93078
Chris Van Patten <Chris@new-host-4.home>
2012-01-28 17:12:22 -0500

    Cleanup and use new stylsheet_link_tag function
    Also added viewport meta tag for mobile browsers

--- wp-content/themes/themename/head.php
+++ wp-content/themes/themename/head.php
@@ -2,27 +2,33 @@
 	<meta charset="UTF-8">
 	<title><?php wp_title(''); ?></title>
 	
-	<?php /* Credits */ ?>
-	<link rel="author" href="humans.txt">
+	<?php /* Credits */ ?> 
+	<link rel="author" href="/humans.txt">
 	
-	<?php /* CSS */ ?>
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/style.css" media="all">
+	<?php /* CSS (see inc/rach5.php for options) */
+		stylesheet_link_tag('/global.css', true, 0, false);
+	?>
 	
-	<?php /* JavaScript Fun For Everyone! */ ?>
-	<script type="text/javascript" src="http://cdn.vanpattenmedia.com/js/libs/LAB.min.js"></script>
+	<?php /* Viewport for mobile */ ?> 
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	
+	<?php /* JavaScript Fun For Everyone! */ ?> 
+	<script type="text/javascript" src="//cdn.vanpattenmedia.com/js/libs/LAB.min.js"></script>
 	<script type="text/javascript">
 		$LAB
-		.script('http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js').wait()
-		.script('http://cdn.vanpattenmedia.com/js/libs/jquery.placeholder.min.js').wait()
-		.script('http://cdn.vanpattenmedia.com/js/libs/formalize/js/jquery.formalize.min.js').wait()
+		.script('//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js').wait()
+		.script('//cdn.vanpattenmedia.com/js/libs/jquery.placeholder.min.js').wait()
+		.script('//cdn.vanpattenmedia.com/js/libs/formalize/js/jquery.formalize.min.js').wait()
 		.script('<?php bloginfo('template_directory'); ?>/js/script.js').wait();
 	</script>
 	
-	<?php wp_head(); ?>
+	<?php /* WordPress head */
+		wp_head();
+	?>
 	
-	<?php /* OpenGraph */ ?>
+	<?php /* OpenGraph */ ?> 
 	<meta property="og:locale" content="en_US">
-	<meta property="fb:admins" content="1366500013">
+	<meta property="fb:admins" content="<?php /* Your FB ID here */ ?>">
 	<meta property="og:title" content="<?php wp_title(''); ?>">
 	<meta property="og:url" content="<?php bloginfo('url'); ?>">
 	<meta property="og:site_name" content="<?php bloginfo('name'); ?>"/>
@@ -30,18 +36,16 @@
 	<meta property="og:description" content="<?php bloginfo('description'); ?>">
 	<meta property="og:image" content="<?php bloginfo('template_directory'); ?>/img/opengraph.png">
 	
-	<?php /* Analytics */ ?>
+	<?php /* Google Analytics */ ?> 
 	<script type="text/javascript">
-	
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'XX-XXXXXX-XX']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'XX-XXXXXX-XX']);
+		_gaq.push(['_trackPageview']);
+		
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
 	</script>
 </head>
