8510c680421f140c89966dd11db89afe2fa3051b
Chris Van Patten <hello@chrisvanpatten.com>
2012-04-08 02:12:13 -0300

    Changing paths on VPM CDN to include version numbers

--- wp-content/themes/themename/footer.php
+++ wp-content/themes/themename/footer.php
@@ -5,7 +5,7 @@
 	</footer>
 
 	<!--[if lt IE 7 ]>
-	<script src="http://cdn.vanpattenmedia.com/js/libs/dd_belatedpng.js"></script>
+	<script src="http://cdn.vanpattenmedia.com/js/libs/dd_belatedpng/0.0.8a/dd_belatedpng.js"></script>
 	<script> DD_belatedPNG.fix('img, .png_fix');</script>
 	<![endif]-->
 	
