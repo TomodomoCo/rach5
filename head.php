91a5bd9123d5cab0bd59dd0db1eaf7870d757ee5
Chris Van Patten <chris@vanpattenmedia.com>
2012-05-02 13:12:56 -0400

    Stop wasting processing power on including CSS. do it inline.

--- head.php
+++ head.php
@@ -1,26 +1,22 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php wp_title(''); ?></title>
-	
+
 	<?php /* humans.txt credits */
 	?><link rel="author" href="/humans.txt">
-	
+
 	<?php /* CSS */
-	
-	// Global Stylesheet
-	stylesheet_link_tag('/global.css', true, 0, true);
-	
-	// Formalize
-	stylesheet_link_tag('http://cdn.vanpattenmedia.com/js/libs/formalize/1.2/css/formalize.css', false, 1, false); ?>
-	
+	?><link rel="stylesheet" type="text/css" href="/global.css">
+	<link rel="stylesheet" type="text/css" href="http://cdn.vanpattenmedia.com/js/libs/formalize/1.2/css/formalize.css">
+
 	<?php /* Mobile viewport lock */
 	?><meta name="viewport" content="width=device-width; initial-scale=1.0">
-	
+
 	<?php /* HTML5 Shiv for <IE9 */
 	?><!--[if lt IE 9]>
 	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	
+
 	<?php /* JavaScript */
 	?><script type="text/javascript" src="//cdn.vanpattenmedia.com/js/libs/labjs/2.0.3/LAB.min.js"></script>
 	<script type="text/javascript">
@@ -29,24 +25,24 @@
 		.script('//cdn.vanpattenmedia.com/js/libs/formalize/1.2/js/jquery.formalize.min.js').wait()
 		.script('<?php bloginfo('template_directory'); ?>/js/script.js').wait();
 	</script>
-	
+
 	<?php /* WordPress head injection */
 	wp_head(); ?>
-	
+
 	<?php /* Google Analytics */
 	$urlParts = explode('.', $_SERVER['HTTP_HOST']);
 	if ($urlParts[0] != 'staging') : ?>
-	
+
 	<script type="text/javascript">
 		var _gaq = _gaq || [];
-		
+
 		_gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
 		_gaq.push(['_trackPageview']);
-		
+
 		(function() {
 			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		})();
-	</script><?php endif; ?> 
+	</script><?php endif; ?>
 </head>
