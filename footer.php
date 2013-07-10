f3891e690a1ec0299b394fb289381ce1d2aa6f30
Chris Van Patten <hello@chrisvanpatten.com>
2013-07-10 15:59:34 -0400

    Remove png polyfill and some tabs

--- footer.php
+++ footer.php
@@ -1,12 +1,7 @@
-	<footer>
-		<div id="legal">
-			<?php copyright('2011'); ?> <?php bloginfo('name'); ?>
-		</div>
-	</footer>
-
-	<!--[if lt IE 7 ]>
-	<script src="http://cdn.vanpattenmedia.com/js/libs/dd_belatedpng/0.0.8a/dd_belatedpng.js"></script>
-	<script> DD_belatedPNG.fix('img, .png_fix');</script>
-	<![endif]-->
+<footer>
+	<div id="legal">
+		<?php copyright('2011'); ?> <?php bloginfo('name'); ?>
+	</div>
+</footer>
 	
 <?php wp_footer(); ?>
