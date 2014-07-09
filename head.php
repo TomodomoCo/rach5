3cb4e6ca55a7e9c13307b23c62a23d51a68b3412
Chris Van Patten <hello@chrisvanpatten.com>
2014-07-09 17:11:17 -0400

    s/;/,

--- head.php
+++ head.php
@@ -1,6 +1,6 @@
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php wp_title(''); ?></title>
 
 	<link rel="author" href="/humans.txt">
